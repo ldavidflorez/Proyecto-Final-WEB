516862f174137e64ec754b3d92bac079406bed32
MauricioDuque <mauricio.duque@udea.edu.co>
2021-10-19 00:40:17 -0500

    Se hace la validación del formulario de compra

--- assets/js/validacion-compra.js
+++ assets/js/validacion-compra.js
@@ -1,6 +1,7 @@
+
 window.onload=function(){
 
-	var formRegistro = document.contact_;
+	var formRegistro=document.contact;
 	console.log(formRegistro);
 
 	var Elementos=formRegistro.elements;
@@ -10,6 +11,7 @@ window.onload=function(){
 			Elementos[i].onkeypress=preValidacion;
 
 		}
+		
 	}
 
 	//VALIDAR
@@ -28,7 +30,7 @@ function preValidacion(event){
  			caracterPermitidos="0123456789";
  			break;
  		case 'email':
- 			caracterPermitidos="@#-abcdefghijklmnopqrstuvwxyz1234567890.";
+ 			caracterPermitidos="@abcdefghijklmnopqrstuvwxyz1234567890.";
  			break;
  	}
 	var letra=String.fromCharCode(event.charCode);
@@ -36,23 +38,63 @@ function preValidacion(event){
 
 }
 
-function validar(){
-    var Departamentos = ['antioquia', 'arauca', 'bolivar', 'caqueta', 'cundinamarca', 'amazonas', 
-                        'atlantico', 'boyaca', 'caldas', 'casanare', 'cauca', 'cesar', 'choco',
-                        'cordoba', 'huila', 'guajira', 'magdalena', 'meta', 'nariño', 'norte de santander',
-                        'putumayo', 'quindio', 'risaralda', 'san adres', 'santander', 'sucre', 'tolima',
-                        'vichada', 'vaupes'];
-	var nombre=this.nombre.value
-    var apellido=this.apellido.value;
-	var correo=this.correo.value;
-    var departamento=this.departamento.value.toLowerCase();
-    var municipio=this.municipio.value;
-    var postal=this.postal.value;
-    var direccion=this.direccion.value;
-    var telefono=this.phone.value;
-    var genero=this.select;
-    var index=genero.selectedIndex; //accedo a la posicion seleccionada
-    var seleccion=genero.options[index]; //accedo al elemento seleccionado
+ function validar(){
+	var nombre=this.name.value;
+	var apellido=this.lastname.value;
+	var correo=this.email.value;
+	var departamento=this.departamento.value.toLowerCase();
+	var municipio=this.municipio.value;
+	var telefono=this.phone.value;
+	var postal=this.postal.value;
+	var genero=this.select;
+	var direccion=this.direction.value;
+	var index=genero.selectedIndex; //accedo a la posicion seleccionada
+	var seleccion=genero.options[index]; //accedo al elemento seleccionado
+	var Departamentos = ['antioquia', 'arauca', 'bolivar', 'caqueta', 'cundinamarca', 'amazonas', 
+	'atlantico', 'boyaca', 'caldas', 'casanare', 'cauca', 'cesar', 'choco',
+	'cordoba', 'huila', 'guajira', 'magdalena', 'meta', 'nariño', 'norte de santander',
+	'putumayo', 'quindio', 'risaralda', 'san adres', 'santander', 'sucre', 'tolima',
+	'vichada', 'vaupes'];
+	
+	// Conditions
+	//1.Campos llenos
+	if (nombre == '' || correo == '' || telefono == '' || apellido == ''|| departamento == ''|| municipio=='' || postal==''|| direccion=='') {
+		alert("Todos los campos son requeridos.....!");
+		return false;
+	}
+	//Verificar departamento
+	else if  (!Departamentos.includes(departamento)){
+		alert("El departamento ingresado no es valido");
+		return false;
+	}
+	//N° telefonico
+	else if  (telefono.length<7){
+		alert("El telefono debe tener al menos 7 digitos");
+		return false;
+	}
+	//Seleccion de genero
+	else if (seleccion.value=='0') {
+		alert("Usted no ha seleccionado el género!");
+		return false;
+	} 
+	//Codigo postal
+	else if  (postal.length<6){
+		alert("El codigo postal debe tener como minimo 6 digitos ");
+		return false;
+	}
+	else{
+		alert("Su formulario se ha enviado correctamente");
+		return true;
+	}
+
+} 
+/* function validar(){
+    
+
+
+
+
+
 	var mensaje=this.mensaje.value;
 	
 	// Conditions
@@ -81,4 +123,4 @@ function validar(){
 		return true;
 	}
 
-}
+} */
